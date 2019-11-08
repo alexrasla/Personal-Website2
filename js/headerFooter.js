b83efe1ade5af02945454fed4f4d264da06e2643
alexrasla <alexrasla@ucsb.edu>
2019-11-08 15:52:23 -0800

    about me header added, styling should work

--- js/headerFooter.js
+++ js/headerFooter.js
@@ -1,5 +1,6 @@
 
 $( function load() {
     $("#header").load("/views/headerFooter/header.html");
+    $("#abt_header").load("/views/headerFooter/header_aboutme.html");
     $("#footer").load("/views/headerFooter/footer.html");
 });
